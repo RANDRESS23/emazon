c4cbb01ee645648d7777ad9cb79e88863a49d8b2
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-10-10 11:27:54 -0500

    feat: add category panel route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryPanelComponent } from './pages/profile/admin/category-panel/category-panel.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'profile/admin/category-panel', component: CategoryPanelComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
