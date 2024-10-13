1f0148a87516a02ba63d2869600e547459119e4b
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-10-13 17:34:22 -0500

    feat: add new constants

--- src/app/shared/domain/constants/admin.ts
+++ src/app/shared/domain/constants/admin.ts
@@ -1,20 +1,27 @@
+import { BrandResponse } from "../interfaces/brand";
 import { CategoryResponse } from "../interfaces/category";
 
 export const EMPTY_STRING = '';
 
 export const ADD_CATEGORY_BUTTON_TEXT = 'Añadir categoría';
+export const ADD_BRAND_BUTTON_TEXT = 'Añadir marca';
 export const ADD_CATEGORY_CLOSE_ICON_PATH = '/assets/icons/close-icon.svg';
 export const ADD_CATEGORY_CLOSE_ICON_ALT = 'Icono de cerrar';
 
 export const SAVE_CATEGORY_BUTTON_TEXT = 'Guardar categoría';
+export const SAVE_BRAND_BUTTON_TEXT = 'Guardar marca';
 export const SAVING_BUTTON_TEXT = 'Guardando...';
 export const CANCEL_BUTTON_TEXT = 'Cancelar';
 
 export const REGISTER_NEW_CATEGORY_TEXT = 'Registrar nueva';
 export const REGISTER_NEW_CATEGORY_TEXT_PRIMARY = 'categoría';
+export const REGISTER_NEW_BRAND_TEXT = 'Registrar nueva';
+export const REGISTER_NEW_BRAND_TEXT_PRIMARY = 'marca';
 
-export const MAX_LENGTH_NAME_FIELD = 50;
-export const MAX_LENGTH_DESCRIPTION_FIELD = 90;
+export const MAX_LENGTH_CATEGORY_NAME_FIELD = 50;
+export const MAX_LENGTH_BRAND_NAME_FIELD = 50;
+export const MAX_LENGTH_CATEGORY_DESCRIPTION_FIELD = 90;
+export const MAX_LENGTH_BRAND_DESCRIPTION_FIELD = 120;
 
 export const NAME_CATEGORY_INPUT_LABEL = 'Nombre categoría';
 export const NAME_CATEGORY_INPUT_PLACEHOLDER = 'Nombre';
@@ -22,6 +29,13 @@ export const NAME_CATEGORY_INPUT_NAME = 'name';
 export const DESCRIPTION_CATEGORY_TEXTAREA_LABEL = 'Descripción categoría';
 export const DESCRIPTION_CATEGORY_TEXTAREA_PLACEHOLDER = 'Descripción';
 export const DESCRIPTION_CATEGORY_TEXTAREA_NAME = 'description';
+
+export const NAME_BRAND_INPUT_LABEL = 'Nombre marca';
+export const NAME_BRAND_INPUT_PLACEHOLDER = 'Nombre';
+export const NAME_BRAND_INPUT_NAME = 'name';
+export const DESCRIPTION_BRAND_TEXTAREA_LABEL = 'Descripción marca';
+export const DESCRIPTION_BRAND_TEXTAREA_PLACEHOLDER = 'Descripción';
+export const DESCRIPTION_BRAND_TEXTAREA_NAME = 'description';
 
 export const REQUIRED_FIELD_ERROR_TEXT = 'El campo es requerido';
 export const MAX_LENGTH_FIELD_ERROR_TEXT = 'El campo debe tener un máximo de caracteres de';
@@ -34,9 +48,12 @@ export const ARROW_LEFT_ICON_PATH = '/assets/icons/arrow-left-icon.svg';
 export const ARROW_RIGHT_ICON_PATH = '/assets/icons/arrow-right-icon.svg';
 
 export const CATEGORY_SAVED_TEXT = 'La categoría fue guardada con éxito';
+export const BRAND_SAVED_TEXT = 'La marca fue guardada con éxito';
 
 export const TABLE_HEADERS_CATEGORY = ['ID', 'Nombre', 'Descripción'];
+export const TABLE_HEADERS_BRAND = ['ID', 'Nombre', 'Descripción'];
 export const CATEGORY_KEYS: (keyof CategoryResponse)[] = ['categoryId', 'name', 'description'];
+export const BRAND_KEYS: (keyof BrandResponse)[] = ['brandId', 'name', 'description'];
 
 export const INITIAL_PAGE_TABLE = 0;
 export const INITIAL_TOTAL_PAGE_TABLE = 0;
