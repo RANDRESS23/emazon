6edaf7197a3b0c958ecc056b4a57c077134df0f7
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-10-13 23:01:22 -0500

    refactor: change small details to table component

--- src/app/shared/ui/organisms/table/table.component.ts
+++ src/app/shared/ui/organisms/table/table.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { BrandResponse } from '@src/app/shared/domain/interfaces/brand';
 import { CategoryResponse } from '@src/app/shared/domain/interfaces/category';
 
 @Component({
@@ -7,9 +8,9 @@ import { CategoryResponse } from '@src/app/shared/domain/interfaces/category';
   styleUrls: ['./table.component.scss']
 })
 export class TableComponent implements OnInit {
-  @Input() list: CategoryResponse[] = [];
+  @Input() list: (CategoryResponse | BrandResponse)[] = [];
   @Input() headers: string[] = [];
-  @Input() keys: (keyof CategoryResponse)[] = [];
+  @Input() keys: (keyof (CategoryResponse | BrandResponse))[] = [];
 
   constructor() { }
 
