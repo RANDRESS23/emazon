98159ce3a8024749004125917b6b893a138dbbbf
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-10-15 22:11:02 -0500

    feat: add product panel route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CategoryPanelComponent } from './pages/profile/admin/category-panel/category-panel.component';
 import { BrandPanelComponent } from './pages/profile/admin/brand-panel/brand-panel.component';
+import { ProductPanelComponent } from './pages/profile/admin/product-panel/product-panel.component';
 
 const routes: Routes = [
   { path: 'profile/admin/category-panel', component: CategoryPanelComponent },
-  { path: 'profile/admin/brand-panel', component: BrandPanelComponent }
+  { path: 'profile/admin/brand-panel', component: BrandPanelComponent },
+  { path: 'profile/admin/product-panel', component: ProductPanelComponent }
 ];
 
 @NgModule({
