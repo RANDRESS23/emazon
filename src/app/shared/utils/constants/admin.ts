8c8d5e95dbfb5b8429857b01bf2eae90e999dfc1
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-10-22 10:06:24 -0500

    feat: add new constants

--- src/app/shared/utils/constants/admin.ts
+++ src/app/shared/utils/constants/admin.ts
@@ -1,19 +1,27 @@
 import { ProductResponse } from "@utils/interfaces/product";
 import { BrandResponse } from "../interfaces/brand";
 import { CategoryResponse } from "../interfaces/category";
+import { WarehouseAssistantResponse } from "@utils/interfaces/warehouse-assistant";
 
 export const EMPTY_STRING = '';
 export const ZERO = 0;
+export const NAME_REGEX = "^[A-Za-záéíóúÁÉÍÓÚñÑüÜ\s]+$";
+export const DOCUMENT_REGEX = "^\\d{7,10}$";
+export const PHONE_REGEX = "^\\+?\\d{1,12}$";
+export const EMAIL_REGEX = "^[\\w!#$%&'*+/=?`{|}~^.-]+@[\\w.-]+\\.[a-zA-Z]{2,}$";
+export const PASSWORD_REGEX = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[!@#$%^&*()_+])[A-Za-z\\d!@#$%^&*()_+]{8,}$";
 
 export const ADD_CATEGORY_BUTTON_TEXT = 'Añadir categoría';
 export const ADD_BRAND_BUTTON_TEXT = 'Añadir marca';
 export const ADD_PRODUCT_BUTTON_TEXT = 'Añadir producto';
+export const ADD_WAREHOUSE_ASSISTANT_BUTTON_TEXT = 'Añadir auxiliar de bodega';
 export const ADD_CATEGORY_CLOSE_ICON_PATH = '/assets/icons/close-icon.svg';
 export const ADD_CATEGORY_CLOSE_ICON_ALT = 'Icono de cerrar';
 
 export const SAVE_CATEGORY_BUTTON_TEXT = 'Guardar categoría';
 export const SAVE_BRAND_BUTTON_TEXT = 'Guardar marca';
 export const SAVE_PRODUCT_BUTTON_TEXT = 'Guardar producto';
+export const SAVE_WAREHOUSE_ASSISTANT_BUTTON_TEXT = 'Guardar usuario';
 export const SAVING_BUTTON_TEXT = 'Guardando...';
 export const CANCEL_BUTTON_TEXT = 'Cancelar';
 
@@ -23,6 +31,8 @@ export const REGISTER_NEW_BRAND_TEXT = 'Registrar nueva';
 export const REGISTER_NEW_BRAND_TEXT_PRIMARY = 'marca';
 export const REGISTER_NEW_PRODUCT_TEXT = 'Registrar nuevo';
 export const REGISTER_NEW_PRODUCT_TEXT_PRIMARY = 'producto';
+export const REGISTER_NEW_WAREHOUSE_ASSISTANT_TEXT = 'Registrar';
+export const REGISTER_NEW_WAREHOUSE_ASSISTANT_TEXT_PRIMARY = 'auxiliar de bodega';
 
 export const MAX_LENGTH_CATEGORY_NAME_FIELD = 50;
 export const MAX_LENGTH_CATEGORY_DESCRIPTION_FIELD = 90;
@@ -68,10 +78,46 @@ export const CATEGORIES_PRODUCT_INPUT_LABEL = 'Categorías producto';
 export const CATEGORIES_PRODUCT_INPUT_LABEL2 = 'categorías';
 export const CATEGORIES_PRODUCT_INPUT_NAME = 'categories';
 
+export const NAME_WAREHOUSE_ASSISTANT_INPUT_LABEL = 'Nombres completos';
+export const NAME_WAREHOUSE_ASSISTANT_INPUT_LABEL2 = 'Nombres';
+export const NAME_WAREHOUSE_ASSISTANT_INPUT_NAME = 'name';
+
+export const LAST_NAME_WAREHOUSE_ASSISTANT_INPUT_LABEL = 'Apellidos completos';
+export const LAST_NAME_WAREHOUSE_ASSISTANT_INPUT_LABEL2 = 'Apellidos';
+export const LAST_NAME_WAREHOUSE_ASSISTANT_INPUT_NAME = 'lastName';
+
+export const DOCUMENT_WAREHOUSE_ASSISTANT_INPUT_LABEL = 'Documento de identidad';
+export const DOCUMENT_WAREHOUSE_ASSISTANT_INPUT_LABEL2 = 'Documento';
+export const DOCUMENT_WAREHOUSE_ASSISTANT_INPUT_NAME = 'document';
+
+export const PHONE_WAREHOUSE_ASSISTANT_INPUT_LABEL = 'Número de celular';
+export const PHONE_WAREHOUSE_ASSISTANT_INPUT_LABEL2 = 'Celular';
+export const PHONE_WAREHOUSE_ASSISTANT_INPUT_NAME = 'phone';
+
+export const BIRTHDATE_WAREHOUSE_ASSISTANT_INPUT_LABEL = 'Fecha de nacimiento';
+export const BIRTHDATE_WAREHOUSE_ASSISTANT_INPUT_LABEL2 = 'Fecha de nacimiento';
+export const BIRTHDATE_WAREHOUSE_ASSISTANT_INPUT_NAME = 'birthdate';
+
+export const EMAIL_WAREHOUSE_ASSISTANT_INPUT_LABEL = 'Correo electrónico';
+export const EMAIL_WAREHOUSE_ASSISTANT_INPUT_LABEL2 = 'Correo';
+export const EMAIL_WAREHOUSE_ASSISTANT_INPUT_NAME = 'email';
+
+export const PASSWORD_WAREHOUSE_ASSISTANT_INPUT_LABEL = 'Contraseña';
+export const PASSWORD_WAREHOUSE_ASSISTANT_INPUT_LABEL2 = 'Contraseña';
+export const PASSWORD_WAREHOUSE_ASSISTANT_INPUT_NAME = 'password';
+
 export const REQUIRED_FIELD_ERROR_TEXT = 'El campo es requerido';
 export const MAX_LENGTH_FIELD_ERROR_TEXT = 'El campo debe tener un máximo de caracteres de';
 export const MIN_VALUE_FIELD_ERROR_TEXT = 'El valor tiene que ser mayor o igual a 1';
 export const SERVER_ERROR_TEXT = 'Error inesperado al guardar en el servidor';
+export const PATTERN_ERRORS = {
+  name: 'Los nombres solo puede contener letras',
+  lastName: 'Los apellidos solo puede contener letras',
+  document: 'El documento debe tener entre 7 y 10 dígitos',
+  phone: 'El número de celular debe tener un máximo de 13 dígitos, y puede contener el simbolo "+"',
+  email: 'El correo debe tener un formato válido',
+  password: 'La contraseña debe tener al menos 8 caracteres, una letra mayúscula, una letra minúscula, un número y un carácter especial'
+}
 
 export const SUCCESS_ICON_PATH = '/assets/icons/success-icon.svg';
 export const ERROR_ICON_PATH = '/assets/icons/error-icon.svg';
@@ -82,6 +128,7 @@ export const ARROW_RIGHT_ICON_PATH = '/assets/icons/arrow-right-icon.svg';
 export const CATEGORY_SAVED_TEXT = 'La categoría fue guardada con éxito';
 export const BRAND_SAVED_TEXT = 'La marca fue guardada con éxito';
 export const PRODUCT_SAVED_TEXT = 'El producto fue guardado con éxito';
+export const WAREHOUSE_ASSISTANT_SAVED_TEXT = 'El auxiliar de bodega fue registrado con éxito';
 
 export const TABLE_HEADERS_CATEGORY = [
   {
@@ -141,9 +188,40 @@ export const TABLE_HEADERS_PRODUCT = [
     isCentered: false,
   }
 ]
+export const TABLE_HEADERS_WAREHOUSE_ASSISTANT = [
+  {
+    label: 'ID',
+    isCentered: true,
+  },
+  {
+    label: 'Nombres',
+    isCentered: false,
+  },
+  {
+    label: 'Apellidos',
+    isCentered: false,
+  },
+  {
+    label: 'Documento',
+    isCentered: false,
+  },
+  {
+    label: 'email',
+    isCentered: false,
+  },
+  {
+    label: 'phone',
+    isCentered: false,
+  },
+  {
+    label: 'birthdate',
+    isCentered: false,
+  }
+]
 export const CATEGORY_KEYS: (keyof CategoryResponse)[] = ['categoryId', 'name', 'description'];
 export const BRAND_KEYS: (keyof BrandResponse)[] = ['brandId', 'name', 'description'];
 export const PRODUCT_KEYS: (keyof ProductResponse)[] = ['productId', 'name', 'description', 'quantity', 'price', 'brand', 'categories'];
+export const WAREHOUSE_ASSISTANT_KEYS: (keyof WarehouseAssistantResponse)[] = ['userId', 'name', 'lastName', 'document', 'email', 'phone', 'birthdate'];
 
 export const INITIAL_PAGE_TABLE = 0;
 export const INITIAL_TOTAL_PAGE_TABLE = 0;
@@ -158,3 +236,4 @@ export const LABEL_SORT_BY_DROPDOWN = 'Ordenar por';
 export const OPTIONS_SHOW_BY_DROPDOWN: Record<string, string | number>[] = [{ label: '10', value: 10 }, { label: '20', value: 20 }, { label: '50', value: 50 }];
 export const OPTIONS_SORT_ORDER_DROPDOWN: Record<string, string | number>[] = [{ label: 'Ascendente', value: 'asc' }, { label: 'Descendete', value: 'desc' }];
 export const PRODUCT_OPTIONS_SORT_BY_DROPDOWN: Record<string, string | number>[] = [{ label: 'Nombre producto', value: 'name' }, { label: 'Nombre marca', value: 'brand' }, { label: 'Nombre categoría', value: 'categories' }];
+export const WAREHOUSE_ASSISTANT_OPTIONS_SORT_BY_DROPDOWN: Record<string, string | number>[] = [{ label: 'Nombre', value: 'name' }, { label: 'Documento', value: 'document' }, { label: 'Correo', value: 'email' }];
