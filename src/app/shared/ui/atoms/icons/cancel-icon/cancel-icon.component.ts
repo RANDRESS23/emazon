c75ddbac0ba5cdfff28ac331ce3b1e71bf6cd5bb
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-10-23 16:17:01 -0500

    feat: add new icons

---
+++ src/app/shared/ui/atoms/icons/cancel-icon/cancel-icon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'atom-cancel-icon',
+  templateUrl: './cancel-icon.component.html',
+  styleUrls: ['./cancel-icon.component.scss']
+})
+export class CancelIconComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
