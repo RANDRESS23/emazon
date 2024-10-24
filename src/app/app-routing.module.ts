174c00db7a3de627f3fc4a0dc434d0410dc71a82
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-10-24 16:35:39 -0500

    feat: add supply panel page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,8 @@ import { WarehouseAssistantPanelComponent } from './pages/profile/admin/warehous
 import { LoginComponent } from './pages/login/login.component';
 import { AuthenticatedGuard } from './core/guards/authenticated/authenticated.guard';
 import { AdminGuard } from './core/guards/admin/admin.guard';
+import { SupplyPanelComponent } from './pages/profile/warehouse-assistant/supply-panel/supply-panel.component';
+import { WarehouseAssistantGuard } from './core/guards/warehouse-assistant/warehouse-assistant.guard';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent, canActivate: [AuthenticatedGuard] },
@@ -14,7 +16,10 @@ const routes: Routes = [
   { path: 'perfil/admin/panel-categoria', component: CategoryPanelComponent, canActivate: [AdminGuard] },
   { path: 'perfil/admin/panel-marca', component: BrandPanelComponent, canActivate: [AdminGuard] },
   { path: 'perfil/admin/panel-producto', component: ProductPanelComponent, canActivate: [AdminGuard] },
-  { path: 'perfil/admin/panel-auxiliar-bodega', component: WarehouseAssistantPanelComponent, canActivate: [AdminGuard] }
+  { path: 'perfil/admin/panel-auxiliar-bodega', component: WarehouseAssistantPanelComponent, canActivate: [AdminGuard] },
+  { path: 'perfil/auxiliar-bodega/inicio', component: SupplyPanelComponent, canActivate: [WarehouseAssistantGuard] },
+  { path: 'perfil/auxiliar-bodega/panel-suministro', component: SupplyPanelComponent, canActivate: [WarehouseAssistantGuard] },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
 
 @NgModule({
