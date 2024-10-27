b2c819320d9918d4627e9972ceed6d83c78b3e5f
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-10-27 12:00:04 -0500

    feat: add new icons

---
+++ src/app/shared/ui/atoms/icons/brand-icon/brand-icon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'atom-brand-icon',
+  templateUrl: './brand-icon.component.html',
+  styleUrls: ['./brand-icon.component.scss']
+})
+export class BrandIconComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
