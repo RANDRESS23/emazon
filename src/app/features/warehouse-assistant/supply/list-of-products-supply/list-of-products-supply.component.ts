e67f9d1d7efa8c20fa245ffb314b5d431b53ddf9
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-10-29 11:39:24 -0500

    feat: add some functionalities in the list of products supply

--- src/app/features/warehouse-assistant/supply/list-of-products-supply/list-of-products-supply.component.ts
+++ src/app/features/warehouse-assistant/supply/list-of-products-supply/list-of-products-supply.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '@src/app/core/services/product/product.service';
-import { INITIAL_PAGE_TABLE, INITIAL_SIZE_ELEMENTS_TABLE, INITIAL_SORT_BY_ELEMENTS_TABLE, INITIAL_SORT_ORDER_ELEMENTS_TABLE, INITIAL_TOTAL_ELEMENTS_TABLE, INITIAL_TOTAL_PAGE_TABLE, PRODUCT_KEYS, PRODUCT_OPTIONS_SORT_BY_DROPDOWN, TABLE_HEADERS_PRODUCT } from '@utils/constants/admin';
+import { INITIAL_PAGE_TABLE, INITIAL_SIZE_ELEMENTS_TABLE, INITIAL_SORT_BY_ELEMENTS_TABLE, INITIAL_SORT_ORDER_ELEMENTS_TABLE, INITIAL_TOTAL_ELEMENTS_TABLE, INITIAL_TOTAL_PAGE_TABLE, PRODUCT_OPTIONS_SORT_BY_DROPDOWN, PRODUCT_SUPPLY_KEYS, TABLE_HEADERS_PRODUCT_SUPPLY } from '@utils/constants/admin';
+import { EMPTY_STRING } from '@utils/constants/general';
 import { PageProducts, ProductResponse } from '@utils/interfaces/product';
+import { ProductSupply } from '@utils/interfaces/supply';
 
 @Component({
   selector: 'app-list-of-products-supply',
@@ -14,16 +16,27 @@ export class ListOfProductsSupplyComponent implements OnInit {
   sortOrder: string = INITIAL_SORT_ORDER_ELEMENTS_TABLE;
   sortBy: string = INITIAL_SORT_BY_ELEMENTS_TABLE;
   optionsSortByDropdown: Record<string, string | number>[] = PRODUCT_OPTIONS_SORT_BY_DROPDOWN;
-  listOfProducts: ProductResponse[] = [];
+  listOfProducts: ProductSupply[] = [];
+  productId: string = EMPTY_STRING;
   totalPages: number = INITIAL_TOTAL_PAGE_TABLE;
   totalElements: number = INITIAL_TOTAL_ELEMENTS_TABLE;
-  headers: Record<string, string | boolean>[] = TABLE_HEADERS_PRODUCT;
-  keys: (keyof ProductResponse)[] = PRODUCT_KEYS as (keyof (ProductResponse))[];
+  headers: Record<string, string | boolean>[] = TABLE_HEADERS_PRODUCT_SUPPLY;
+  keys: (keyof ProductSupply)[] = PRODUCT_SUPPLY_KEYS as (keyof (ProductSupply))[];
+  showModal: () => void = () => {};
 
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
     this.getProducts(this.pageNumber, this.size, this.sortOrder, this.sortBy);
+  }
+
+  showModalOutput(onShowModal: () => void): void {
+    this.showModal = onShowModal;
+  }
+
+  showModalWithProductId(item: any) {
+    this.productId = item.productId;
+    this.showModal();
   }
 
   changePage(pageNumber: number): void {
@@ -35,7 +48,14 @@ export class ListOfProductsSupplyComponent implements OnInit {
   getProducts(pageNumber: number, size: number, sortOrder: string, sortBy: string): void {
     this.productService.getAllProducts(pageNumber, size, sortOrder, sortBy).subscribe({
       next: (pageProducts: PageProducts) => {
-        this.listOfProducts = pageProducts.content;
+        const products = pageProducts.content.map((product: ProductResponse) => {
+          return { productId: product.productId, name: product.name, quantity: product.quantity, accion: {
+            icon: 'add',
+            accion: this.showModalWithProductId.bind(this)
+          }}
+        })
+
+        this.listOfProducts = products;
         this.totalPages = pageProducts.totalPages;
         this.totalElements = pageProducts.totalElements;
       },
@@ -45,8 +65,7 @@ export class ListOfProductsSupplyComponent implements OnInit {
     })
   }
 
-  addNewProductCount(): void {
-    this.totalElements = this.totalElements++;
+  addNewSupplyCount(): void {
     this.getProducts(this.pageNumber, this.size, this.sortOrder, this.sortBy);
   }
 
