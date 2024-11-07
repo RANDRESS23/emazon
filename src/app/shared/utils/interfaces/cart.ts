0f505484f46732c0eea4a3b68a28ee70167add7d
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-11-07 14:27:49 -0500

    feat: add new cart interfaces

---
+++ src/app/shared/utils/interfaces/cart.ts
@@ -0,0 +1,24 @@
+export interface CartProduct {
+  cartProductId: number;
+  cartId: number;
+  productId: number;
+  name: string;
+  quantity: number;
+  unitPrice: number;
+  totalPrice: number;
+} 
+
+export interface Cart {
+  cartId: number;
+  clientId: number;
+  totalQuantity: number;
+  totalPrice: number;
+  createdAt: Date | string;
+  updatedAt: Date | string;
+  products: CartProduct[];
+}
+
+export interface CartProductRequest {
+  productId: number;
+  quantity: number;
+}
