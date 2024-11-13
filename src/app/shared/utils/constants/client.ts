fc52a1e2b131e500e1d68c62674e60daad9c65ee
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-11-13 10:23:50 -0500

    feat: add new client constants

--- src/app/shared/utils/constants/client.ts
+++ src/app/shared/utils/constants/client.ts
@@ -3,6 +3,7 @@ export const INITIAL_SIZE_PRODUCTS = 20;
 export const PRODUCT_SAVED_IN_CART = 'Producto agregado al carrito de compras';
 export const PRODUCT_REMOVED_IN_CART = 'Producto eliminado del carrito de compras';
 export const PRODUCTS_REMOVED_IN_CART = 'Productos eliminados del carrito de compras';
+export const SUCCESSFUL_PURCHASE = 'Compra realizada exitosamente';
 
 export const ERROR_CLIENT_NOT_LOGGED = 'Debes iniciar sesión para acceder a tu carrito de compras';
 export const ERROR_CLIENT_NOT_LOGGED2 = 'Debes iniciar sesión para agregar productos a tu carrito de compras';
@@ -15,3 +16,5 @@ export const LABEL_FILTER_MARCA_DROPDOWN = 'Marca';
 
 export const ALL_CATEGORIES_TEXT = 'Todas las categorías';
 export const ALL_BRANDS_TEXT = 'Todas las marcas';
+
+export const BUY_PRODUCTS_BUTTON_TEXT = 'Comprar productos';
