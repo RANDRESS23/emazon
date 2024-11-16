fa6101fe2e94caefb1022026fd4dfc9505fc8514
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-11-15 21:47:05 -0500

    refactor: change small details

--- src/app/features/client/cart/cart-products/cart-products.component.ts
+++ src/app/features/client/cart/cart-products/cart-products.component.ts
@@ -1,4 +1,3 @@
-import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '@src/app/shared/services/cart/cart.service';
 import { ToastService } from '@src/app/shared/services/toast/toast.service';
