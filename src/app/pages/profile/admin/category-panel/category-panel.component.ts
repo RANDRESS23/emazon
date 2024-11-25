2d708c22da6a59ac3a8395fcba2c102292fd5993
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-11-24 21:36:07 -0500

    feat: add some details in the brand, category and product panels

--- src/app/pages/profile/admin/category-panel/category-panel.component.ts
+++ src/app/pages/profile/admin/category-panel/category-panel.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { MENU_ITEMS_ADMIN } from '@utils/constants/general';
+import { AuthService } from '@src/app/core/services/auth/auth.service';
+import { MENU_ITEMS_ADMIN, MENU_ITEMS_WAREHOUSE_ASSISTANT } from '@utils/constants/general';
+import { RolesEnum } from '@utils/enums/roles';
 
 @Component({
   selector: 'app-category-panel',
@@ -10,8 +12,9 @@ export class CategoryPanelComponent implements OnInit {
   menuItems: Record<string, string>[] = MENU_ITEMS_ADMIN;
   isAdminLogged: boolean = true;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.menuItems = this.authService.getRole() === RolesEnum.ADMIN ? MENU_ITEMS_ADMIN : MENU_ITEMS_WAREHOUSE_ASSISTANT;
   }
 }
